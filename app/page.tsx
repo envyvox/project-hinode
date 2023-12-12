74db5c277266f69b993bed60a7cf717f53959fc7
envyvox <envyvox@gmail.com>
2023-12-12 03:43:26 +0200

    added link to dashboard on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex flex-col p-24">
-      <span>Main Content</span>
+    <main className="flex flex-col items-center p-24">
+      <Link className={buttonVariants()} href="/dashboard">
+        Start playing
+      </Link>
     </main>
   );
 }
