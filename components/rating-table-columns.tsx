879bb1dd23b0eb9fa21f113deb63d3bc83e875de
envyvox <envyvox@gmail.com>
2023-12-17 07:17:40 +0200

    cleanup

--- components/rating-table-columns.tsx
+++ components/rating-table-columns.tsx
@@ -9,46 +9,38 @@ export const ratingTableColumns: ColumnDef<User>[] = [
   {
     accessorKey: "position",
     header: "rating.user.position",
-    cell: ({ row }) => {
-      return (
-        <div className="flex items-center gap-2 text-center">
-          {displayPosition(row.index + 1)}
-        </div>
-      );
-    },
+    cell: ({ row }) => (
+      <div className="flex items-center gap-2 text-center">
+        {displayPosition(row.index + 1)}
+      </div>
+    ),
   },
   {
     accessorKey: "name",
     header: "rating.user.name",
-    cell: ({ row }) => {
-      const title = row.original.title;
-
-      return (
-        <div className="flex items-center gap-2">
-          <Image
-            className="h-6 w-6"
-            width={27}
-            height={27}
-            src={`/title/${title}.png`}
-            alt={title}
-          />
-          <TypographyMuted>{title}</TypographyMuted>
-          <span>{title}</span>
-        </div>
-      );
-    },
+    cell: ({ row }) => (
+      <div className="flex items-center gap-2">
+        <Image
+          className="h-6 w-6"
+          width={27}
+          height={27}
+          src={`/title/${row.original.title}.png`}
+          alt={row.original.title}
+        />
+        <TypographyMuted>{row.original.title}</TypographyMuted>
+        <span>{row.original.name}</span>
+      </div>
+    ),
   },
   {
     accessorKey: "level",
     header: "rating.user.level",
-    cell: ({ row }) => {
-      return (
-        <div className="flex items-center gap-2">
-          {displayLevel(row.original.level)}
-          {row.original.level}
-        </div>
-      );
-    },
+    cell: ({ row }) => (
+      <div className="flex items-center gap-2">
+        {displayLevel(row.original.level)}
+        {row.original.level}
+      </div>
+    ),
   },
   {
     accessorKey: "xp",
