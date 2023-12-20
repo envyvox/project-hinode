0c992bd70e8ce90455209aaf4e0c7391d2529801
envyvox <envyvox@gmail.com>
2023-12-20 03:59:02 +0200

    added new user things

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -4,6 +4,9 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/lib/prisma";
 
+const newUserCurrencyAmount = 1000;
+const newUserBannerId = "clqd2zrhy000sxsul7i0v6ytv";
+
 export const options: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -19,5 +22,31 @@ export const options: NextAuthOptions = {
   pages: {
     signIn: "/auth/signin",
     error: "/auth/signin",
+  },
+  events: {
+    async signIn({ user, isNewUser }) {
+      if (isNewUser) {
+        await prisma.userCurrency.create({
+          data: {
+            userId: user.id,
+            currency: "Ien",
+            amount: newUserCurrencyAmount,
+          },
+        });
+        await prisma.userTitle.create({
+          data: {
+            userId: user.id,
+            title: "Newbie",
+          },
+        });
+        await prisma.userBanner.create({
+          data: {
+            userId: user.id,
+            bannerId: newUserBannerId,
+            isActive: true,
+          },
+        });
+      }
+    },
   },
 };
