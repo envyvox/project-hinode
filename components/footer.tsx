db1e8ac125b2eb6aec83c384e1a46ed88b45e2e9
envyvox <envyvox@gmail.com>
2023-12-23 11:42:58 +0200

    fix footer styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { siteConfig } from "@/config/site";
 
 const Footer = () => {
   return (
-    <footer className="sticky bottom-0 py-6 md:px-8 md:py-0">
+    <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           Built by{" "}
