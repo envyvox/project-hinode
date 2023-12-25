a4af5197714e5294cdde3663ebe2c4e884a769a0
envyvox <envyvox@gmail.com>
2023-12-25 16:11:31 +0200

    refactor(inventory): improved skeleton

--- components/inventory/inventory-skeleton.tsx
+++ components/inventory/inventory-skeleton.tsx
@@ -1,12 +1,25 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
+const Component = () => {
+  return (
+    <div className="flex flex-col gap-5 rounded-lg border bg-card p-5 text-card-foreground shadow-sm">
+      <div className="flex gap-5">
+        <Skeleton className="h-12 w-12" />
+        <div className="w-1/2">
+          <Skeleton className="h-[24px] w-full" />
+          <Skeleton className="mt-1 h-[14px] w-1/2" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const InventorySkeleton = () => {
   return (
     <>
-      <Skeleton className="h-[90px]" />
-      <Skeleton className="h-[90px]" />
-      <Skeleton className="h-[90px]" />
-      <Skeleton className="h-[90px]" />
+      <Component />
+      <Component />
+      <Component />
     </>
   );
 };
